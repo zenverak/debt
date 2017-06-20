6aecdaa53683345a9e6e49c4602347b7e4bb7882
Chris Jones <cjones343@gatech.edu>
2017-06-20 15:55:08 -0400

    created some models

--- debt_tracker/models.py
+++ debt_tracker/models.py
@@ -2,5 +2,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
+
+
+class Debtor(models.Model):
+    debtor = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    add_date = models.DateTimeField(default=timezone.now)
+    uniqueid = models.IntegerField()
+
+    def __str__(self):
+        return self.debtor
+class Debt(models.Model):
+    debtor = models.ForeignKey(Debtor, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    date_added =  models.DateTimeField(default=timezone.now)
+    owed_to = models.CharField(max_length=100)
 
 # Create your models here.
