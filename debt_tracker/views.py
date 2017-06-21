a5b4b76b944cfc326b7b270b087c5b9a5b2513f7
Chris Jones <cjones343@gatech.edu>
2017-06-21 18:16:06 -0400

    created dev branch for thing I can't fix y et

--- debt_tracker/views.py
+++ debt_tracker/views.py
@@ -23,10 +23,26 @@ def debt_list(request, id):
 
 	
 def debt_pay(request,pk):
+    print "PAYING SOME DEBT"
+    debt = Debt.objects.get(pk=pk)
     if request.method == "POST":
-        pass
+#        print "THAT METHOD WAS A POST"
+        form = DebtPay(request.POST)
+        form.fields['amount'].disabled = True
+        info = {"form":form, "debt":debt}
+#        print "THE FORM BEING VALID IS {0}".format(form.is_valid())
+        if form.is_valid():
+#            print "form is valid"
+            payment_amount = form.cleaned_data.get('payment_amount')
+#            print "payment amount is {0} and current debt amount is {1}".format(payment_amount,info['debt'].amount)
+            debt.pay(payment_amount)
+#            print "new debt amount is {0}".format(info['debt'].amount)
+            intial = {'amount': debt.amount, 'payment_amount': 0.0}
+            form = DebtPay(request.POST, initial=initial)
+            return render(request, 'debt_tracker/debt_pay.html', {'info': info})
+        else:
+            return render(request, 'debt_tracker/debt_pay.html', {'info': info})
     else:
-        debt = Debt.objects.get(pk=pk)
         form = DebtPay(initial={'amount':debt.amount})
         form.fields['amount'].disabled = True
         info = {"form":form, "debt":debt}
