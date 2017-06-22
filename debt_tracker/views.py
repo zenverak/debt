74ca458877edf85430faa3b3572cc1d00be032d8
Chris Jones <cjones343@gatech.edu>
2017-06-22 15:08:16 -0400

    added the ability to add debts and add debtors via forms. Changed it so that paid debts don't show

--- debt_tracker/views.py
+++ debt_tracker/views.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Debtor, Debt
-from .forms import DebtPay
+from .forms import DebtPay,DebtForm, DebtorForm
 
 def debtor_list(request):
     debtors = Debtor.objects.all()
@@ -16,22 +16,22 @@ def debt_list(request, id):
     debtor and his debts
     '''
     debtor = Debtor.objects.get(uniqueid=id)
-    debts = Debt.objects.filter(debtor=debtor)
+    debts = Debt.objects.filter(debtor=debtor).filter(status='Not Paid')
     info={"debtor":debtor,"debts":debts}
     return render(request, 'debt_tracker/debt_list.html',{'info':info})
 	
 
 	
 def debt_pay(request,pk):
-    print request
+#    print request
 #    print "post is {0}".format(request.POST)
 #    print request.POST['payment_amount']
-    print "PAYING SOME DEBT"
+#    print "PAYING SOME DEBT"
     debt = Debt.objects.get(pk=pk)
     if request.method == "POST":
         debt.pay(float(request.POST['payment_amount']))
         debt.save()
-        print debt.amount
+#        print debt.amount
         form = DebtPay(initial={'amount':debt.amount})
         form.fields['amount'].disabled = True
         info = {"form":form, "debt":debt}
@@ -41,3 +41,28 @@ def debt_pay(request,pk):
         form.fields['amount'].disabled = True
         info = {"form":form, "debt":debt}
         return render(request, 'debt_tracker/debt_pay.html', {'info': info})
+
+def debt_add(request, id):
+    if request.method == "POST":
+        form = DebtForm(request.POST)
+        debt = form.save(commit=False)
+        id = int(request.POST.get('id'))
+        debt.debtor =  Debtor.objects.get(uniqueid=id)
+        debt.save()
+        return redirect('debt_list', id=debt.debtor.uniqueid)
+    else:
+        form = DebtForm()
+        info = {'form':form, 'id':id}
+        return render(request, 'debt_tracker/debt_add.html', {'info':info})
+
+def debtor_add(request):
+    if request.method == "POST":
+        form = DebtorForm(request.POST)
+        debtor = form.save()
+        debtor.save()
+        return redirect('debtor_list')
+    else:
+        form = DebtorForm()
+        return render(request, 'debt_tracker/debtor_add.html', {'form': form})
+        
+    
